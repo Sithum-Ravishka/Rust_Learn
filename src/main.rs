1a108fa06acb9ae69af05c20084139a1d13db85a
sithum-ravishka <sithumravishka1@gmail.com>
2023-10-24 12:08:49 +0530

    if statement

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,50 @@
 use std::io;
 
 fn main() {
-    let a = [1, 2, 3, 4, 5];
-
-    println!("Please enter an array index");
-
-    let mut index = String::new();
+    
+    println!("Please enter number: ");
+    let mut number = String::new();
 
     io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
+        .read_line(&mut number)
+        .expect("Failed to read_line");
 
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
+    let number: u32 = number .trim().parse().expect("Input value is not a number");
 
-    let element = a[index];
-
-    println!("The value of the element at index {index} is: {element}");
+    if ({number} < 5) {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 }
+
+
+
+
+
+
+
+
+
+// use std::io;
+
+// fn main() {
+//     let a = [1, 2, 3, 4, 5];
+
+//     println!("Please enter an array index");
+
+//     let mut index = String::new();
+
+//     io::stdin()
+//         .read_line(&mut index)
+//         .expect("Failed to read line");
+
+//     let index: usize = index
+//         .trim()
+//         .parse()
+//         .expect("Index entered was not a number");
+
+//     let element = a[index];
+
+//     println!("The value of the element at index {index} is: {element}");
+// }
