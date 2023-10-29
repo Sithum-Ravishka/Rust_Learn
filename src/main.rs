09d95f50a58c6b53079b8fa9baa5f841b5ba809a
sithum-ravishka <sithumravishka1@gmail.com>
2023-10-29 16:40:58 +0530

    update main

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,4 @@ fn main() {
         Some(max) => println!("The maximum is configured to be {}", max),
         _ => (),
     }
-
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
 }
