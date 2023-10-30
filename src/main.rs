affbf406240ab65ceda544522c0cee98d6c994d5
sithum-ravishka <sithumravishka1@gmail.com>
2023-10-30 10:56:02 +0530

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
-fn main() {
-    let config_max = Some(54u8);
-    match config_max {
-        Some(max) => println!("The maximum is configured to be {}", max),
-        _ => (),
-    }
+use std::io;
+
+fn main(){
+    println!("Enter First Number: ");
+
+    let mut num1 = String::new();
+
+    io::stdin()
+    .read_line(&mut num1)
+    .expect("msg");
+
+    let num1: u32 = num1.trim().parse().expect("msg");
+
+    println!("Enter secound Number: ");
+
+    let mut num2 = String::new();
+
+    io::stdin()
+    .read_line(&mut num2)
+    .expect("msg");
+
+    let num2: u32 = num2.trim().parse().expect("msg");
+
+    let total = num1 + num2;
+
+    print!("Total value is:  {total}");
+
+
 }
