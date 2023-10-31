225667369697dd5d9b744043e79083774c50110e
sithum-ravishka <sithumravishka1@gmail.com>
2023-10-31 16:01:35 +0530

    Update string file

--- src/hashmap.rs
+++ src/hashmap.rs
@@ -4,9 +4,11 @@ fn main() {
     let mut scores = HashMap::new();
 
     scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 8);
     scores.insert(String::from("Yellow"), 50);
 
-    for (key, value) in &scores {
-        println!("{key}: {value}");
-    }
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(1);
+
+    println!("{score}")
 }
--- src/string_type.rs
+++ src/string_type.rs
@@ -35,3 +35,76 @@ fn main() {
     println!("{s} \n");    // output >>>>  initial contents
 
 }
+
+
+
+
+fn main() {
+    let mut s = String::from("foo");
+    s.push_str(" bar");
+
+
+    println!("{s}");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {s1}");
+
+}
+
+
+
+fn main() {
+    let mut s = String::from("lo");
+    s.push('l');
+
+
+    println!("{s}");
+}
+
+
+
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+    println!("Enter your name : ");
+    
+
+    io::stdin().read_line(&mut s).expect("msg");
+
+
+    let s1 = String::from("Hello, ");
+
+    let s3 = s1 + {&s}; // note s1 has been moved here and can no longer be used
+
+    println!("{s3}");
+}
+
+
+
+
+fn main() {
+    let hello = "Здравствуйте";
+
+    let s = &hello[0..8]; // this work but not work[0..1] and [0..5] like this because there 2 space need string value
+    
+    println!("{s}");
+}
+
+
+
+fn main() {
+    for c in "Зд".chars() {
+        println!("{c}");
+    }
+}
+
+
+
+fn main() {
+    for b in "Зд".bytes() {
+        println!("{b}");
+    }    
+}
