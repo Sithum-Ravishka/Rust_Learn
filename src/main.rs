bea4d8f7c2d6fc70d6a0c7cd2e78b3535d75d632
sithum-ravishka <sithumravishka1@gmail.com>
2023-11-02 10:13:35 +0530

    update impl

--- src/main.rs
+++ src/main.rs
@@ -1,141 +1,38 @@
-use std::collections::HashMap;
-use std::io;
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    // Without `self` keyword as its first parameter, this is an associated function that returns a new instance of `Rectangle`.
+    // `Self` is an alias for the type `Rectangle`.
+    fn new(width: u32, height: u32) -> Self {
+        Self {
+            width,
+            height,
+        }
+    }
+
+    // `&self` borrows an immutable instance of the Rectangle type and reads its fields
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    // `&mut self` borrows a mutable instance of the Rectangle type and can mutate its fields
+    fn change_width(&mut self, new_width: u32) {
+        self.width = new_width;
+    }
+}
 
 fn main() {
-    let mut eng = HashMap::new();
-    let mut sal = HashMap::new();
+    let mut rect = Rectangle::new(30, 50);
 
-    loop {
-        println!("What is department? (Engineering/Sales)");
-        let mut department = String::new();
-        io::stdin()
-            .read_line(&mut department)
-            .expect("Failed to read input");
+    rect.change_width(60);
 
-        let department = department.trim().to_lowercase();
-
-        if department == "engineering" {
-            let mut names: Vec<String> = Vec::new();
-
-            loop {
-                let mut input = String::new();
-
-                println!("Enter a name: ");
-                io::stdin()
-                    .read_line(&mut input)
-                    .expect("Failed to read input");
-
-                let input_trimmed = input.trim().to_lowercase();
-
-                names.push(input_trimmed.to_string());
-
-                println!("Do you want to add another name? (yes/no)");
-                let mut add_more = String::new();
-                io::stdin()
-                    .read_line(&mut add_more)
-                    .expect("Failed to read input");
-
-                let add_more_trimmed = add_more.trim().to_lowercase();
-
-                if add_more_trimmed != "yes" {
-                    eng.entry("Engineering".to_string()).or_insert(Vec::new()).extend(names);
-                    break; // Exit the inner loop
-                }
-            }
-        } else if department == "sales" {
-            let mut sales_dep: Vec<String> = Vec::new();
-
-            loop {
-                let mut input_sales = String::new();
-
-                println!("Enter a name: ");
-                io::stdin()
-                    .read_line(&mut input_sales)
-                    .expect("Failed to read input");
-
-                let input_trimmed = input_sales.trim().to_lowercase();
-
-                sales_dep.push(input_trimmed.to_string());
-
-                println!("Do you want to add another name? (yes/no)");
-                let mut add_more = String::new();
-                io::stdin()
-                    .read_line(&mut add_more)
-                    .expect("Failed to read input");
-
-                let add_more_trimmed = add_more.trim().to_lowercase();
-
-                if add_more_trimmed != "yes" {
-                    sal.entry("Sales".to_string()).or_insert(Vec::new()).extend(sales_dep);
-                    break; // Exit the inner loop
-                }
-            }
-        } else {
-            println!("Invalid department. Please enter 'Engineering' or 'Sales'.");
-        }
-
-        println!("Do you want to add names for another department? (yes/no)");
-        let mut add_department = String::new();
-        io::stdin()
-            .read_line(&mut add_department)
-            .expect("Failed to read input");
-
-        let add_department_trimmed = add_department.trim().to_lowercase();
-
-        if add_department_trimmed != "yes" {
-            break; // Exit the outer loop
-        }
-    }
-
-    loop {
-        println!("Options:");
-        println!("1. Retrieve a list of all people in Engineering (sorted alphabetically)");
-        println!("2. Retrieve a list of all people in Sales (sorted alphabetically)");
-        println!("3. Retrieve a list of all people in the company by department (sorted alphabetically)");
-        println!("4. Exit");
-
-        let mut option = String::new();
-        io::stdin()
-            .read_line(&mut option)
-            .expect("Failed to read input");
-
-        match option.trim() {
-            "1" => {
-                if let Some(names) = eng.get("Engineering") {
-                    let mut sorted_names = names.clone();
-                    sorted_names.sort();
-                    println!("People in Engineering (sorted alphabetically): {:?}", sorted_names);
-                } else {
-                    println!("No names in Engineering.");
-                }
-            }
-            "2" => {
-                if let Some(names) = sal.get("Sales") {
-                    let mut sorted_names = names.clone();
-                    sorted_names.sort();
-                    println!("People in Sales (sorted alphabetically): {:?}", sorted_names);
-                } else {
-                    println!("No names in Sales.");
-                }
-            }
-            "3" => {
-                let mut all_names: Vec<(String, Vec<String>)> = Vec::new();
-                all_names.push(("Engineering".to_string(), eng.get("Engineering").cloned().unwrap_or_default()));
-                all_names.push(("Sales".to_string(), sal.get("Sales").cloned().unwrap_or_default()));
-                all_names.sort_by(|a, b| a.0.cmp(&b.0));
-                for (dept, names) in all_names {
-                    let mut sorted_names = names.clone();
-                    sorted_names.sort();
-                    println!("People in {} (sorted alphabetically): {:?}", dept, sorted_names);
-                }
-            }
-            "4" => {
-                println!("Goodbye!");
-                return;
-            }
-            _ => {
-                println!("Invalid option. Please select a valid option.");
-            }
-        }
-    }
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect.area()
+    );
+    // => The area of the rectangle is 3000 square pixels.
 }
