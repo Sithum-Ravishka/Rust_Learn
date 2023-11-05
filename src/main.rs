c8157ab3f5a5638e95b01511ae15bbbd8ba2e389
sithum-ravishka <sithumravishka1@gmail.com>
2023-11-05 21:55:01 +0530

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,52 @@
-use std::io;
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
 
-fn main() {
-    
-    let mut vec = Vec::new();
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
 
-    println!("Enter 's' to start program!!!");
-    println!("Enter 'q' to quit program!!!");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Faild to read line");
-    input = input.trim().to_string();
-    
-    let mut i = 1;
-    let mut x = 1;
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
 
-    loop {
-        if input == "q" {
-            println!("Quitting program...");
-            break;
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
         }
-
-        match input.trim() {
-            "s" => {
-                println!("Enter name {}: or press 'q' to stop | press 'v' to view list", i);
-                let mut name = String::new();
-                io::stdin().read_line(&mut name).expect("Failed to read the line.");
-                name = name.trim().to_string();
-
-                if name == "q" {
-                    break;
-                }
-
-                if name == "v" {
-                    println!();
-                    println!("List of Names");
-                    println!("-------------");
-
-                    if vec.is_empty() {
-                        println!();
-                        println!("No names in the list");
-                        break;
-                    }
-                    for items in &vec {
-                        println!("{}. {}", x, items);
-                        x += 1;
-                    }
-
-                    println!();
-                    continue;
-                }
-
-                vec.push(name);
-                i += 1;
-            }
-            _ => {
-                println!("Invalid input");
-                break;
-            }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
         }
     }
+}
+
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
 }
