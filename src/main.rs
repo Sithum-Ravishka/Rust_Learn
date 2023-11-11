3dfe7b1c2dde8cd7f806a029e782b8fbfc2eae92
sithum-ravishka <sithumravishka1@gmail.com>
2023-11-11 14:37:06 +0530

    result

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,49 @@
-// Topic: Result & the question mark operator
-//
+// Topic: Strings
 
+// * Print out the name and favorite colors of people aged 10 and under
 
-#[derive(Clone, Copy, Debug)]
-enum ProtectedLocation {
-    All,
-    Office,
-    Warehouse,
-}
-
-impl ProtectedLocation {
-    fn required_access_level(&self) -> u16 {
-        match self {
-            Self::All => 1000,
-            Self::Office => 800,
-            Self::Warehouse => 500,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Database;
-
-impl Database {
-    fn connect() -> Result<Self, String> {
-        // In a production application, a database connection error is likely to occur here.
-        Ok(Database)
-    }
-
-    fn find_employee(&self, name: &str) -> Result<Employee, String> {
-        match name {
-            "Anita" => Ok(Employee {
-                name: "Anita".to_string(),
-            }),
-            "Brody" => Ok(Employee {
-                name: "Brody".to_string(),
-            }),
-            "Catherine" => Ok(Employee {
-                name: "Catherine".to_string(),
-            }),
-            _ => Err(String::from("employee not found")),
-        }
-    }
-
-    fn get_keycard(&self, employee: &Employee) -> Result<KeyCard, String> {
-        match employee.name.as_str() {
-            "Anita" => Ok(KeyCard { access_level: 1000 }),
-            "Brody" => Ok(KeyCard { access_level: 500 }),
-            other => Err(format!("{other} doesn't have a keycard")),
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-struct Employee {
+// * Use a struct for a persons age, name, and favorite color
+struct Persons{
+    age: i32,
+    // * The color and name should be stored as a String
     name: String,
+    fav_color: String
 }
 
-#[derive(Debug)]
-struct KeyCard {
-    access_level: u16,
-}
-
-#[derive(Clone, Copy, Debug)]
-enum AuthorizationStatus {
-    Allow,
-    Deny,
-}
-
-fn authorize(
-    employee_name: &str,
-    location: ProtectedLocation,
-) -> Result<AuthorizationStatus, String> {
-    let db = Database::connect()?;
-    let employee = db.find_employee(employee_name)?;
-    let keycard = db.get_keycard(&employee)?;
-
-    if keycard.access_level >= location.required_access_level() {
-        Ok(AuthorizationStatus::Allow)
-    } else {
-        Ok(AuthorizationStatus::Deny)
-    }
-}
 
 fn main() {
-    // Anita is trying to access the Warehouse, which requires access level 500.
-    // Her keycard has access level 1000, which should be allowed.
-    let anita_authorized = authorize("Anita", ProtectedLocation::Warehouse);
-    // Brody is trying to access the Office, which requires access level 800.
-    // His keycard has access level 500, which should be denied.
-    let brody_authorized = authorize("Brody", ProtectedLocation::Office);
-    // Catherine is trying to access the Warehouse, which requires access level 500.
-    // She doesn't have a keycard, so this should be an error.
-    let catherine_authorized = authorize("Catherine", ProtectedLocation::Warehouse);
+// * Create and store at least 3 people in a vector
+    let persons1 = vec![
+        Persons {
+            age : 12,
+            name: String::from("sithum"),
+            fav_color: String::from("black")
+        },
+        Persons {
+            age : 9,
+            name: String::from("ravishka"),
+            fav_color: String::from("red")
+        },
+        Persons {
+            age : 5,
+            name: String::from("baby"),
+            fav_color: String::from("green")
+        },
+    ];
 
-    println!("{anita_authorized:?}");
-    println!("{brody_authorized:?}");
-    println!("{catherine_authorized:?}");
+    // * Iterate through the vector using a for..in loop
+
+    for list in persons1 {
+    // * Use an if expression to determine which person's info should be printed
+        if list.age <= 10{
+            // * The name and colors should be printed using a function
+            print(&list.name);
+            print(&list.fav_color)
+        }
+    }
+
+    fn print(data: &str){
+        println!("{:?}", data);
+    }
+    
 }
