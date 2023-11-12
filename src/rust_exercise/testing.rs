fe6a66ee329230ddd6ed78054d8f597cd35c1e40
sithum-ravishka <sithumravishka1@gmail.com>
2023-11-12 23:25:20 +0530

    testing

---
+++ src/rust_exercise/testing.rs
@@ -0,0 +1,91 @@
+// Topic: Testing
+
+// Requirements:
+// * Write tests for the existing program to ensure proper functionality.
+
+// * Create at least two test cases for each function.
+// * Use `cargo test` to test the program.
+// * There are intentional bugs in the program that need to be fixed.
+//   * Check the documentation comments for the functions to
+//     determine how the they should operate.
+
+/// Ensures n is >= lower and <= upper.
+fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
+    if n < lower {
+        lower
+    } else if n > upper {
+        upper
+    } else {
+        n
+    }
+}
+
+/// Divides a and b.
+fn div(a: i32, b: i32) -> Option<i32> {
+    if b == 0 {
+        return None;
+    } else {
+        return Some(a / b);
+    }
+}
+
+/// Takes two strings and places them immediately one after another.
+fn concat(first: &str, second: &str) -> String {
+    format!("{} {}", first, second)
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn clamp_lower() {
+        let result = clamp(10, 100, 1000);
+        let expected = 100;
+
+        assert_eq!(result, expected, "msg")
+    }
+
+    #[test]
+    fn clamp_upper() {
+        let result = clamp(5000, 100, 1000);
+        let expected = 1000;
+
+        assert_eq!(result, expected, "msg")
+    }
+
+    #[test]
+
+    fn div_test() {
+        let result = div(100, 20);
+        let expected = 5;
+
+        assert_eq!(result, Some(expected));
+    }
+
+    #[test]
+    fn div_test1() {
+        let result = div(100, 20);
+        let expected = Some(5);
+
+        assert_eq!(result, expected, "msg");
+    }
+
+    #[test]
+    fn div_test_zero() {
+        let result = div(100, 0); //If "b" value is zero result will be None
+        let expected = None;
+
+        assert_eq!(result, expected, "msg");
+    }
+
+    #[test]
+    fn concat_test() {
+        let result = concat("sithum", "ravishka");
+        let expected = "sithum ravishka";
+
+        assert_eq!(result, expected, "msg")
+    }
+}
